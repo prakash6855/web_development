ba7be039c94b8a4467bc8ea8734c3ac504a0771c
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-08 15:21:53 +0530

    new codes added

--- javascript/33 Practiceset10/script.js
+++ javascript/33 Practiceset10/script.js
@@ -23,4 +23,80 @@ if(candrive(age)){
 }
 */
 
-// Question:---In question 1 use confirm to 
+// Question:---In question 1 use confirm to ask user if he wants to see the prompt again.
+
+/* 
+let runagain = true;
+const candrive = (age) => {
+    return age >= 18 ? true : false;
+}
+while (runagain) {
+    let age = prompt("Enter your age");
+    age = Number.parseInt(age);
+    if (candrive(age)) {
+        alert("You can drive");
+    } else {
+        alert("You can not drive");
+    }
+    runagain = confirm("Do you want to play again");
+}
+*/
+
+
+// Question:---In the previous question use console.error to log an error if the age entered is negative
+/*
+let age = prompt("Enter your age");
+age = Number.parseInt(age);
+
+if(age >= 18){
+  alert("Yes, you can drive");
+}else if(age < 18 && age >= 0){
+  alert("You are too young");
+} else{
+    console.error("Invalid age");
+}
+*/
+
+/*
+let runagain = true;
+const candrive = (age) => {
+    return age >= 18 ? true : false;
+}
+while (runagain) {
+    let age = prompt("Enter your age");
+    age = Number.parseInt(age);
+    if (age < 0) {
+        console.error("Age will never be negative.Try agein");
+        break;
+    }
+    if (candrive(age)) {
+        alert("You can drive");
+    } else {
+        alert("You can not drive");
+    }
+    runagain = confirm("Do you want to play again");
+}
+*/
+
+
+//Question:----Write a program to change the url to google.com if user enters a number greater than 4
+
+/*
+let num = prompt("Enter a number");
+num = Number.parseInt(num);
+
+const guessednum = (num) => {
+  return num > 4 ? true : false;
+}
+if (guessednum(num)){
+  location.href = "https://www.google.com";
+}else{
+  console.log("Try again");
+}
+*/
+
+
+// Change the background of the page to yellow or red or any color basec on user choice through prompt
+
+let color = prompt("Enter color of your choice");
+document.body.style.background = color;
