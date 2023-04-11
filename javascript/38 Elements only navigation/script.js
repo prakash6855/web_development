e31a6d7f7e948c4e25d282be49cba1120913a4dd
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-11 12:01:51 +0530

    new codes added

--- javascript/38 Elements only navigation/script.js
+++ javascript/38 Elements only navigation/script.js
@@ -1,3 +1,6 @@
+const changeBgRed = ()=>{
+document.body.firstElementChild.style.background = "red"
+}
 let b = document.body;
 console.log("first child of b is: ", b.firstChild);
 console.log("first element child of b is: ", b.firstElementChild);
