1e6fa0948be66ec4861853e197fd7c00b74a1aca
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-11 11:45:50 +0530

    midified the code sucessfully

--- javascript/38 Elements only navigation/script.js
+++ javascript/38 Elements only navigation/script.js
@@ -1,7 +1,7 @@
 let b = document.body;
 console.log("first child of b is: ", b.firstChild);
 console.log("first element child of b is: ", b.firstElementChild);
-console.log(document.previousElementSibling);
-console.log(document.nextElementSibling);
-console.log(document.firstElementChild);
-console.log(document.lastElementChild);
+console.log(b.previousElementSibling);
+console.log(b.nextElementSibling);
+console.log(b.firstElementChild);
+console.log(b.lastElementChild);
