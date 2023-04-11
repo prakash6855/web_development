a6eb751fe36c409947de2dd45497517b0ae31a53
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-11 13:22:35 +0530

    new codes added

---
+++ javascript/39 Table Navigation/script.js
@@ -0,0 +1,10 @@
+let t = document.body.firstElementChild.firstElementChild;
+console.log(t);
+console.log(t.rows);
+console.log(t.caption);
+console.log(t.tHead);
+console.log(t.tFoot);
+console.log(t.tHead.firstElementChild);
+console.log(t.tBodies);
+console.log(t.tBodies.rows);
+console.log(t.rows[1].rowIndex);
