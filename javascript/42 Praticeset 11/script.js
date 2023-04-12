00158266f228a392a2e0dfa3c89518e1722f187e
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-12 19:08:12 +0530

    modification done

--- javascript/42 Praticeset 11/script.js
+++ javascript/42 Praticeset 11/script.js
@@ -1,9 +1,9 @@
 // Create a nav bar and change the color of its first element to red.
-/*
+
 let input = document.getElementById("elem");
 input.style.color="red";
 
-*/
+
 
 //Create a table without tbody.Now use "view page source" button to check whether it has a tbody or not??
 
@@ -12,14 +12,21 @@ input.style.color="red";
 
 // Create an element with 3 children. Now change the color of first and the last element to green.
 
-document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green"
-document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green"
+let parent = document.querySelector(".navclass");
+console.log(parent);
+console.log(parent.children);
+parent.children[0].style.color ="green";
+parent.children[4].style.color = "green";
+
+
+// document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green"
+// document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green"
 
 
 //Write js code to change bg to all <li> tags to cyan.
-Array.from(document.getElementsByTagName("li")).forEach((element) =>{
-    element.style.background = "cyan";
-  })
+// Array.from(document.getElementsByTagName("li")).forEach((element) =>{
+//     element.style.background = "cyan";
+//   })
 
 
   //Which of the following is used for the farthest ancesstor that matches a given css selector
