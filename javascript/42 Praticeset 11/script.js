cdcedc0566feec2f7ce1469d3b62598561b6109e
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-12 16:57:58 +0530

    new codes added

---
+++ javascript/42 Praticeset 11/script.js
@@ -0,0 +1,34 @@
+// Create a nav bar and change the color of its first element to red.
+/*
+let input = document.getElementById("elem");
+input.style.color="red";
+
+*/
+
+//Create a table without tbody.Now use "view page source" button to check whether it has a tbody or not??
+
+// No in view page source it will not present because it shows what you have written wherein the browser if we inspect it it will present in table
+
+
+// Create an element with 3 children. Now change the color of first and the last element to green.
+
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green"
+document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green"
+
+
+//Write js code to change bg to all <li> tags to cyan.
+Array.from(document.getElementsByTagName("li")).forEach((element) =>{
+    element.style.background = "cyan";
+  })
+
+
+  //Which of the following is used for the farthest ancesstor that matches a given css selector
+//   1.Matches
+//   2.Closest
+//   3.Contains
+//   4.NOTA
+//Ans- NOTA 
+
+
+
+     
