4f822b06c08138b525f1c139b90aa9aaf4ce2580
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-13 00:14:51 +0530

    new codes added

---
+++ javascript/44 Inner HTML and Outer HTML/script.js
@@ -0,0 +1,46 @@
+/*
+
+let p = document.getElementsByTagName("span")[0]
+console.log(p);
+let q = document.getElementsByTagName("span")[0];
+console.dir(q);
+
+console.log(document.body.firstChild.nodeName);
+console.log(document.body.firstElementChild.nodeName);
+
+*/
+// Bellow are the codes when we write in the console of the browser then we will get the outcome as commented bellow
+/*
+first.innerHTML
+//'<b>Hiii there </b>Enjoy the weekend!'
+first.innerHTML = "<i>This week i will do only coding</i>"
+//'<i>This week i will do only coding</i>'
+first.outerHTML
+//'<span id="first"><i>This week i will do only coding</i></span>'
+first.outerHTML = "<div>Viola!!!!you won</div>"
+//'<div>Viola!!!!you won</div>'
+*/
+
+/*
+document.body.firstChild;
+"Diving to JS "
+document.body.firstChild.data;
+'Diving to JS\n  '
+document.body.firstChild.nodeValue;
+'Diving to JS\n  
+  */
+
+
+// If we want text only we can use the bellow code
+// console.log(document.body.textContent);
+// Diving to JS
+  // Hiii there Enjoy the weekend!
+  
+
+/*
+
+If we want to hide something we can use the bellow code
+first.hidden = false
+false
+
+*/
