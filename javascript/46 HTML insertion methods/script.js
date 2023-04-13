c85ea9f07069310ed27cd93c5c292f2b3ad0b605
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-13 17:08:54 +0530

    codes added

---
+++ javascript/46 HTML insertion methods/script.js
@@ -0,0 +1,19 @@
+let a = document.getElementsByTagName('div')[0]
+
+// a.innerHTML = a.innerHTML + '<h1>Hello! Welcome to the world of coding</h1>';
+let div = document.createElement('div');
+// div.innerHTML = '<h1>Hiii!there</h1>'
+
+
+// appendChild() method:------The appendChild() method of the Node interface adds a node to the end of the list of children of a specified parent node. If the given child is a reference to an existing node in the document, appendChild() moves it from its current position to the new position.
+  // a.appendChild(div);
+// append() method:----The Element.append() method inserts a set of Node objects or string objects after the last child of the Element. String objects are inserted as equivalent Text nodes.
+// a.append(div);
+// prepend() method:-----The Element.prepend() method inserts a set of Node objects or string objects before the first child of the Element. String objects are inserted as equivalent Text nodes.
+// a.prepend(div);
+// before() method:----The Element.before() method inserts a set of Node or string objects in the children list of this Element's parent, just before this Element. String objects are inserted as equivalent Text nodes.
+// a.before(div);
+// after() method:----The Element.after() method inserts a set of Node or string objects in the children list of the Element's parent, just after the Element. String objects are inserted as equivalent Text nodes.
+// a.after(div);
+// replaceWith() method:-----The Element.replaceWith() method replaces this Element in the children list of its parent with a set of Node or string objects. String objects are inserted as equivalent Text nodes.
+// a.replaceWith(div);
