9ad1cfd7538824f7b3d1d3ac7b73e341e70b5ab2
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-18 12:05:51 +0530

    New codes added

---
+++ javascript/62 Error handling_Try_Catch/script.js
@@ -0,0 +1,22 @@
+// try...catch:---The try...catch statement is comprised of a try block and either a catch block, a finally block, or both. The code in the try block is executed first, and if it throws an exception, the code in the catch block will be executed. The code in the finally block will always be executed before control flow exits the entire construct.
+
+setTimeout(()=>{
+    console.log("Hacking wifi")
+}, 1000);
+try{
+    console.log(user);
+}
+catch(error){
+console.log(error);
+}
+
+setTimeout(() => {
+    console.log("Hacking personal data started");
+}, 1500);
+setTimeout(() => {
+     console.log("Hacking username and password");
+ }, 2500);
+ setTimeout(() => {
+    console.log("username and password of user is +919253851267....fetching done");
+}, 3500);
+
