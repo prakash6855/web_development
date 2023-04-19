ab4feb37b565ed44778c58057c7d973936cf687d
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-20 00:12:48 +0530

    Modification done

--- javascript/72 Session storage/script.js
+++ javascript/72 Session storage/script.js
@@ -1,28 +1,39 @@
-let key = prompt("Enter the key");
-let value = prompt("Enter the value");
+// let key = prompt("Enter the key");
+// let value = prompt("Enter the value");
 // sessionStorage.setItem():----Stores one or more items in the storage area. If the item exists, its value is updated.
 
-sessionStorage.setItem(key, value);
+// sessionStorage.setItem(key, value);
 
 // sessionStorage.getItem():----Retrieves one or more items from the storage area.
 
-console.log(`The value of ${key} is ${sessionStorage.getItem(key)}`);
+// console.log(`The value of ${key} is ${sessionStorage.getItem(key)}`);
 
 
 // sessionStorage.removeItem():----- Removes one or more items from the storage area.
 
-if (key == "red" || key == "pink"){
-    console.log(sessionStorage.removeItem(key));
-}
+// if (key == "red" || key == "pink"){
+//     console.log(sessionStorage.removeItem(key));
+// }
 
 // sessionStorage.clear() method:-----The clear() method of the Storage interface clears all keys stored in a given Storage object. 
-if(key == 0){
-    sessionStorage.clear();
-}
+
+// if(key == 0){
+//     sessionStorage.clear();
+// }
 
 // sessionStorage.key() method:-----Gives the key on the given position
-sessionStorage.key(0)//0 here is index value
+// sessionStorage.key(0)//0 here is index value
 
 //sessionStorage.length:------This method describes the number of stored items
-sessionStorage.length;
+// sessionStorage.length;
 
+
+
+
+//StorageEvent:-----The StorageEvent interface is implemented by the storage event, which is sent to a window when a storage area the window has access to is changed within the context of another document. 
+
+
+window.onstorage= (event)=>{
+alert("changed");
+console.log(event);
+}
