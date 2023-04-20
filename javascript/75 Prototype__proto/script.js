e5ef374b188bd1b5f787c30bc921fcf9284c2e50
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-20 23:17:23 +0530

    Few changes done

--- javascript/75 Prototype__proto/script.js
+++ javascript/75 Prototype__proto/script.js
@@ -1,3 +1,4 @@
+// Every object in JavaScript has a built-in property, which is called its prototype. The prototype is itself an object, so the prototype will have its own prototype, making what's called a prototype chain. The chain ends when we reach a prototype that has null for its own prototype.
 let a = {
     name: "peter",
 religion: "christian",
