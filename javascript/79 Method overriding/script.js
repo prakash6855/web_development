e0b0d4916a99d46d52906d22d68cef7ed2a32286
prakash6855 <prakashkumar68553495@gmail.com>
2023-04-23 12:14:56 +0530

    Modified codes

--- javascript/79 Method overriding/script.js
+++ javascript/79 Method overriding/script.js
@@ -1,3 +1,4 @@
+/*
 class employee{
    login(){
 console.log(`Employee has logged in`)
@@ -16,5 +17,33 @@ console.log(`Employee has requested for ${x+1} leaves (one bonus)`)
 }
 
 let E = new manager()
+E.login();
 E.logout();
 E.requestLeave(4);
+*/
+
+// super:-----The super keyword is used to access properties on an object literal or class's [[Prototype]], or invoke a superclass's constructor.
+
+class employee {
+    login() {
+        console.log(`Employee has logged in`)
+    }
+    logout() {
+        console.log(`Employee has logged out`)
+    }
+    requestLeave(leaves) {
+        console.log(`Employee has requested for ${leaves} leaves - Auto approved`)
+    }
+}
+class manager extends employee {
+    requestLeave(leaves) {
+        super.requestLeave(10);
+        console.log("One extra granted")
+    }
+}
+
+let E = new manager()
+E.login();
+E.logout();
+E.requestLeave();
+
