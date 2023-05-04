677ad74f58ffddd73027f6b11d6bcf2db1c05693
prakash6855 <prakashkumar68553495@gmail.com>
2023-05-04 14:59:13 +0530

    changes done

--- javascript/86 Destructuring assignment and spread operator/script.js
+++ javascript/86 Destructuring assignment and spread operator/script.js
@@ -9,7 +9,7 @@ let {a, b} = {a:3, b:7};
 // console.log(a, b);
 
 
-//Spread operator:----
+//Spread operator:---- The spread (...) syntax allows an iterable, such as an array or string, to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected. In an object literal, the spread syntax enumerates the properties of an object and adds the key-value pairs to the object being created.
 
 let arr1 = [3, 5, 7]
 let obj1 = {... arr1};
